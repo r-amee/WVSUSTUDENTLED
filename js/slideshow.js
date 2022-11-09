70587c892677e82e0a83db90e24381838e7a8dac
unknown <russel.yasol@gmail.com>
2022-11-09 19:39:23 +0800

    first commit

---
+++ js/slideshow.js
@@ -0,0 +1,12 @@
+function first(){
+    document.getElementById("slide").src="/img/design/place1.png"     
+}
+function second(){
+    document.getElementById("slide").src="/img/design/place2.png"     
+}
+function third(){
+    document.getElementById("slide").src="/img/design/place3.png"     
+}
+setInterval(first,8000);
+setInterval(second,12000);
+setInterval(third,16000);
